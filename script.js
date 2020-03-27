6d322e0fbf3d6850c8926c5473d1f47a67a1b38b
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2020-03-26 21:29:54 -0500

    added pool of characters array

--- script.js
+++ script.js
@@ -17,61 +17,61 @@ function generatePassword() {
 	// Initialize empty string to start password on
 	var pwTentative = "";
 	// array to check desired character type requests against
-	var checkArray = ["true,true,true,true", "true,true,true,false", "true,true,false,true", "true,true,false,false", "true,false,true,true", "true,false,true,false", "true,false,false,true", "true,false,false,false", "false,true,true,true", "false,true,true,false", "false,true,false,true", "false,true,false,false", "false,false,true,true", "false,false,true,false", "false,false,false,true", "false,false,false,false"];
+	var checkArray = ["true,true,true,true", "true,true,true,false", "true,true,false,true", "true,true,false,false", "true,false,true,true", "true,false,true,false", "true,false,false,true", "true,false,false,false", "false,true,true,true", "false,true,true,false", "false,true,false,true", "false,true,false,false", "false,false,true,true", "false,false,true,false", "false,false,false,true"];
 
 	// Go through a loop to add a character for the length of the password
-	for(var i = 0; i < pwLength; i++) {
-		switch(checkArray.indexOf(charTypesChosen.toString())) {
-			case 0:
-				var asciiChooser = Math.floor(Math.random() * 94) + 33;
-				pwTentative = pwTentative + String.fromCharCode(asciiChooser);
+	
+	switch(checkArray.indexOf(charTypesChosen.toString())) {
+		case 0:
+			for(var i = 0; i < pwLength; i++) {
+				var pool = ["!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ":", ";", "<", "=", ">", "?", "@", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "[", "\\", "]", "^", "_", "`", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "{", "|", "}", "~"];
+				var characterChooser = Math.floor(Math.random() * pool.length);
+				pwTentative = pwTentative + pool[characterChooser];
+			}
 			break;
-			case 1:
-				return 1;
+		case 1:
+			return 1;
 			break;
-			case 2:
-				return 2;
+		case 2:
+			return 2;
 			break;
-			case 3:
-				return 3;
+		case 3:
+			return 3;
 			break;
-			case 4:
-				return 4;
+		case 4:
+			return 4;
 			break;
-			case 5:
-				return 5;
+		case 5:
+			return 5;
 			break;
-			case 6:
-				return 6;
+		case 6:
+			return 6;
 			break;
-			case 7:
-				return 7;
+		case 7:
+			return 7;
 			break;
-			case 8:
-				return 8;
+		case 8:
+			return 8;
 			break;
-			case 9:
-				return 9;
+		case 9:
+			return 9;
 			break;
-			case 10:
-				return 10;
+		case 10:
+			return 10;
 			break;
-			case 11:
-				return 11;
+		case 11:
+			return 11;
 			break;
-			case 12:
-				return 12;
+		case 12:
+			return 12;
 			break;
-			case 13:
-				return 13;
+		case 13:
+			return 13;
 			break;
-			case 14:
-				return 14;
+		case 14:
+			return 14;
 			break;
-			case 15:
-				return 15;
-			break;
-		}
+		
 	}
 
 	return pwTentative;
