93f64cfb8d7817fb52791dfcc3098ad595ac1f93
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2020-03-29 12:48:53 -0500

    made char set arrays to simplify pool

--- script.js
+++ script.js
@@ -16,14 +16,19 @@ function generatePassword() {
 	var charTypesChosen = characterTypes();
 	// Initialize empty string to start password on
 	var pwTentative = "";
-	// array to check desired character type requests against
+	// Array to check desired character type requests against
 	var checkArray = ["true,true,true,true", "true,true,true,false", "true,true,false,true", "true,true,false,false", "true,false,true,true", "true,false,true,false", "true,false,false,true", "true,false,false,false", "false,true,true,true", "false,true,true,false", "false,true,false,true", "false,true,false,false", "false,false,true,true", "false,false,true,false", "false,false,false,true"];
+	// Arrays for each charachter type
+	var lowers = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+	var uppers = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+	var nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+	var specials = ["!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
 
-	// Go through a loop to add a character for the length of the password
-	
+	// Check which type of password to make
 	switch(checkArray.indexOf(charTypesChosen.toString())) {
 		case 0:
-			var pool = ["!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ":", ";", "<", "=", ">", "?", "@", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "[", "\\", "]", "^", "_", "`", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "{", "|", "}", "~"];
+			// Go through a loop to add a character for the length of the password	
+			var pool = [...lowers, ...uppers, ...nums, ...specials];
 			for(var i = 0; i < pwLength; i++) {
 				pwTentative = pwTentative + pool[Math.floor(Math.random() * pool.length)];
 			}
